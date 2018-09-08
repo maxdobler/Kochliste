8a96e884d5d92793be6b773e19fa2293537e9d1a
Max Dobler <max.dobler@posteo.de>
2018-09-08 20:53:00 +0200

    textinput in bootstrap

--- src/form/TextInput.js
+++ src/form/TextInput.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
+import {
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  HelpBlock
+} from "react-bootstrap";
+
 export default class TextInput extends Component {
   handleChange = event => this.props.onTextChange(event.target.value);
 
   render() {
     return (
-      <label>
-        {this.props.label}:
-        <input
+      <FormGroup controlId={this.props.label}>
+        <ControlLabel>{this.props.label}</ControlLabel>
+        <FormControl
           type="text"
           value={this.props.text}
+          placeholder={this.props.placeholder || ""}
           onChange={this.handleChange}
         />
-      </label>
+        <FormControl.Feedback />
+      </FormGroup>
     );
   }
 }
